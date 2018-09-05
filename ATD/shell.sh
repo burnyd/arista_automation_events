c22341bfa8a703453734a8390b37030cdb93d8d7
Daniel Hertzberg <burnyd@gmail.com>
2018-09-05 10:03:52 -0400

    Added shell file

---
+++ ATD/shell.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Todos
+#loop through adding directories?
+#Clone the directory or add the salt swix/startup.sh files.
+
+echo "Checking connectivity exists"
+ping -q -c5 google.com > /dev/null
+ 
+if [ $? -eq 0 ]
+then
+	echo "connectivity is there"
+else    echo "connectivity is not there"
+        exit 1 
+fi 
+
+echo "Adding salt directories"
+sudo mkdir -p /srv/salt/
+sudo mkdir -p /srv/salt/pillar/
+sudo mkdir -p /srv/salt/states/
+sudo mkdir -p /srv/salt/templates/
+sudo mkdir -p /srv/salt/states/leaf
+sudo mkdir -p /srv/salt/states/spine
+sudo mkdir -p /srv/salt/reactor/
+
+echo "fixing the default ubuntu pyOpenSSL issue"
+pip install --upgrade pyOpenSSL
+
+echo "Install napalm EOS libraries"
+sudo pip install napalm
+sudo pip install napalm-logs
+sudo pip install napalm-base
+
+echo "Installing Saltstack"
+sudo wget -O bootstrap-salt.sh https://bootstrap.saltstack.com/
+sudo sh bootstrap-salt.sh
+
+sleep 5
+
+echo "restarting the master service" 
+service salt-master restart
+
