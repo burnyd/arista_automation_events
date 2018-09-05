21136f69653ac50b2b84625a95ed893cde96d387
Daniel Hertzberg <burnyd@gmail.com>
2018-09-05 11:14:36 -0400

    added saltstack agent

--- ATD/shell.sh
+++ ATD/shell.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #Todos
 #loop through adding directories?
+#Git clone && cd into directory then shell into this script to run the ansible playbook last?
 #Clone the directory or add the salt swix/startup.sh files.
+
+echo "Firewall rules for salt"
+sudo ufw allow 4505:4506/tcp
 
 echo "Checking connectivity exists"
 ping -q -c5 google.com > /dev/null
