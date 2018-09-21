9b5d4830b13af533efe2448d21578de9e1537b23
root <root@labaccess.arista.lab>
2018-09-21 12:28:39 -0500

    more spelling mistakes

--- ATD/labs/python/jinja2-templates/jinja.py
+++ ATD/labs/python/jinja2-templates/jinja.py
@@ -2,11 +2,12 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 import sys
 
-env = Environment(load=FileSystemLoad('templates'))
+env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('vlans.j2')
 vlan_data = yaml.load(open('vlans.yaml'))
-output = template.render(vlan)data)
+output = template.render(vlan_data)
 with open("config", "wb") as fh:
     fh.write(output)
 
 print output
+
