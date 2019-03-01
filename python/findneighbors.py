28ee870782bbe6a779a0a4f5bba99cd2c29be5b5
burnyd <burnyd@gmail.com>
2019-03-01 09:09:48 -0500

    Added httapi connection modules

--- python/findneighbors.py
+++ python/findneighbors.py
@@ -2,13 +2,13 @@
 
 from jsonrpclib import Server
 
-switchusername = 'vagrant'
-switchpassword = 'vagrant'
-switches = ['leaf1a']
+switchusername = 'ansible'
+switchpassword = 'vansible'
+switches = ['10.20.30.25']
 
 def find_neighbors(): #Function makes sure that each and every API is actually running on each switches and can pull with the current password.
     for switch in switches:
-        urlString = "http://%s:%s@%s/command-api" % (switchusername, switchpassword, switch)
+        urlString = "https://%s:%s@%s/command-api" % (switchusername, switchpassword, switch)
         switchReq = Server( urlString )
         response = switchReq.runCmds( 1, ["show lldp neighbors"] )
         neighborresponse = response[0]["lldpNeighbors"]
