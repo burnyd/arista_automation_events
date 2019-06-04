681b5518dc2f24d8334d72b388091de1fff6fcfd
Daniel Hertzberg <burnyd@gmail.com>
2019-06-04 12:12:11 -0400

    add ansible j2 filters

--- python/findneighbors.py
+++ python/findneighbors.py
@@ -8,7 +8,7 @@ switches = ['base-lab_Spine1']
 
 def find_neighbors(): #Function makes sure that each and every API is actually running on each switches and can pull with the current password.
     for switch in switches:
-        urlString = "https://%s:%s@%s/command-api" % (switchusername, switchpassword, switch)
+        urlString = "http://%s:%s@%s/command-api" % (switchusername, switchpassword, switch)
         switchReq = Server( urlString )
         response = switchReq.runCmds( 1, ["show lldp neighbors"] )
         neighborresponse = response[0]["lldpNeighbors"]
