7372fbb7a7c4347b28796ede951c6803d8e01e4f
Daniel Hertzberg <burnyd@gmail.com>
2019-08-27 13:26:33 -0400

    Made changes to add salt to docker, pillars and base-lab for new ceos

---
+++ salt/master_install.sh
@@ -0,0 +1,25 @@
+apt-get update && apt-get install -y git python-pip python-dev default-jre libffi-dev libssl-dev python-dev python-cffi libxslt1-dev python-pip
+pip install --upgrade pyOpenSSL
+
+ mkdir -p /srv/salt/
+ mkdir -p /srv/salt/pillar/
+ mkdir -p /srv/salt/states/
+ mkdir -p /srv/salt/templates/
+ mkdir -p /srv/salt/states/leaf
+ mkdir -p /srv/salt/states/spine
+ mkdir -p /srv/salt/reactor/
+ mkdir -p /etc/salt
+cp salt/scripts/salt/master /etc/salt/
+
+ pip install napalm
+ pip install napalm-logs
+ pip install napalm-base
+
+ wget -O bootstrap-salt.sh https://bootstrap.saltstack.com/stable
+sh bootstrap-salt.sh -M
+
+
+wget -qO - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
+apt-get install apt-transport-https ca-certificates -y
+echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" |  tee -a /etc/apt/sources.list.d/saltstack.list
+apt-get update && apt-get install salt-master salt-minion -y
--- salt/pillar/top.sls
+++ salt/pillar/top.sls
@@ -1,3 +1,13 @@
 base:
-  '*':
-    - bgp-as
+  {% for index in range(1, 3) -%}
+  'base-lab_Spine{{ index }}':
+    - spine{{ index }}
+  {% endfor -%}
+  'base-lab_Lab1':
+    - leaf1
+  'base-lab_Lab2':
+    - leaf2
+  'base-lab_Lab3':
+    - leaf3
+  'base-lab_Lab4':
+    - leaf4
